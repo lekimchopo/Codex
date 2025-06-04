7322d95285e0a29865be4a2056905c0df837ea35
lekimchopo <lekimchoporomo@gmail.com>
2025-06-04 13:10:09 +0200

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,27 +1,27 @@
 import unittest
-import calculator
+from calculator import suma, resta, multiplicar, dividir
 
 
 class TestCalculadora(unittest.TestCase):
     def test_suma(self):
-        self.assertEqual(calculator.suma(2, 3), 5)
-        self.assertEqual(calculator.suma(-1, 1), 0)
+        self.assertEqual(suma(2, 3), 5)
+        self.assertEqual(suma(-1, 1), 0)
 
     def test_resta(self):
-        self.assertEqual(calculator.resta(10, 5), 5)
-        self.assertEqual(calculator.resta(0, 4), -4)
+        self.assertEqual(resta(10, 5), 5)
+        self.assertEqual(.resta(0, 4), -4)
 
     def test_multiplicar(self):
-        self.assertEqual(calculator.multiplicar(3, 4), 12)
-        self.assertEqual(calculator.multiplicar(-2, 3), -6)
+        self.assertEqual(.multiplicar(3, 4), 12)
+        self.assertEqual(multiplicar(-2, 3), -6)
 
     def test_dividir(self):
-        self.assertEqual(calculator.dividir(10, 2), 5)
-        self.assertAlmostEqual(calculator.dividir(7, 2), 3.5)
+        self.assertEqual(dividir(10, 2), 5)
+        self.assertAlmostEqual(dividir(7, 2), 3.5)
 
     def test_dividir_por_cero(self):
         with self.assertRaises(ZeroDivisionError):
-            calculator.dividir(1, 0)
+            dividir(1, 0)
 
 
 if __name__ == '__main__':
